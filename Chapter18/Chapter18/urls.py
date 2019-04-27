89e6bcfe06d946ef60ace6dc765d5fde80069970
Chris Cho <isi.cho@gmail.com>
2019-04-28 06:24:12 +0900

    add page app

--- Chapter18/Chapter18/urls.py
+++ Chapter18/Chapter18/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('hello/', include('hello.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('page/', include('page.urls')),
 ]
